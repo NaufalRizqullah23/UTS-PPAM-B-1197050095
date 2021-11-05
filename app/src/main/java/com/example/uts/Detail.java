58064392a4102bf5cad2b54de329690de467506e
NaufalRizqullah23 <naufalrizqullah23@Gmail.com>
2021-11-05 13:49:05 +0700

    project UTS PPAM

---
+++ app/src/main/java/com/example/uts/Detail.java
@@ -0,0 +1,36 @@
+package com.example.uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        int id = getIntent().getIntExtra("id",-1);
+
+        ArrayList<Food> list = FoodData.getData(getApplicationContext());
+        Food food = list.get(id);
+
+        if(food != null){
+            TextView judulPan = findViewById(R.id.detail_judul);
+            TextView hargaPan = findViewById(R.id.detail_harga);
+            TextView descPan = findViewById(R.id.detail_desc);
+            ImageView image = findViewById(R.id.detail_image);
+
+            image.setImageDrawable(food.gambar);
+            judulPan.setText(food.title);
+            hargaPan.setText(String.valueOf(food.price));
+            descPan.setText(food.price);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/uts/FoodData.java
@@ -0,0 +1,19 @@
+package com.example.uts;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.Menu;
+
+import java.util.ArrayList;
+
+public class FoodData{
+    public static ArrayList<Food> getData(Context context){
+        ArrayList<Food> list = new ArrayList<Food>();
+        list.add(new Food("Cheese Cake","Desert Dish",35000, context.getDrawable(R.drawable.cheesecake)));
+        list.add(new Food("Cireng","Fried Side Dish",2000,context.getDrawable(R.drawable.cireng)));
+        list.add(new Food("Donut","Who doesn't love donuts?",5000,context.getDrawable(R.drawable.donut)));
+        list.add(new Food("Mie Goreng","Indomie Seleraku!",8000,context.getDrawable(R.drawable.mie_goreng)));
+        list.add(new Food("Kopi Hitam","Kopi Hitam Asli Indonesia",7500,context.getDrawable(R.drawable.kopi_hitam)));
+        return list;
+    }
+}
