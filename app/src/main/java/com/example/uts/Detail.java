42d477c8da9f5b66154703c71edc911664250643
NaufalRizqullah23 <naufalrizqullah23@Gmail.com>
2021-11-05 14:29:56 +0700

    Project UTS PPAM

--- app/src/main/java/com/example/uts/Detail.java
+++ app/src/main/java/com/example/uts/Detail.java
@@ -29,7 +29,7 @@ public class Detail extends AppCompatActivity {
             image.setImageDrawable(food.gambar);
             judulPan.setText(food.title);
             hargaPan.setText(String.valueOf(food.price));
-            descPan.setText(food.price);
+            descPan.setText(food.descript);
 
         }
     }
